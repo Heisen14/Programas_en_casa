412356fe432d758d7346f860def07b4a5c028597
Heisen14 <danilo_01@outlok.es>
2018-04-27 19:35:14 -0500

    Programa no apto para jotos

---
+++ Programa1.py
@@ -0,0 +1,11 @@
+nombre=input("Ingresa tu nombre")
+apellidoPaterno=input("ingresa tu apellido paterno")
+apellidoMaterno=input("ingresa tu apellido materno")
+
+nombreCompleto=[nombre,apellidoPaterno,apellidoMaterno]
+if " ".join(nombreCompleto) == "Joaquin Aragón Pineda":
+    print(nombre+"chinga tu madre, eres un maricón")
+else:
+    print(" ".join(nombreCompleto))
+
+
