927552a40820d383db99122d962dbe630cbcdac6
Heisen14 <danilo_01@outlok.es>
2018-04-27 19:53:18 -0500

    Broma para hermano y motivación personal

--- Programa1.py
+++ Programa1.py
@@ -1,3 +1,4 @@
+print("Escribe con buena ortografía así como con signos de puntuación")
 
 nombre=input("Ingresa tu nombre")
 apellidoPaterno=input("ingresa tu apellido paterno")
@@ -6,6 +7,8 @@ apellidoMaterno=input("ingresa tu apellido materno")
 nombreCompleto=[nombre,apellidoPaterno,apellidoMaterno]
 if " ".join(nombreCompleto) == "Joaquin Aragón Pineda":
     print(nombre+" chinga tu madre, eres un maricón")
+elif " ".join(nombreCompleto) == "Daniel Aragón Pineda":
+    print(nombre+" eres una chingonería y puedes hacer todo lo que te propongas")
 else:
     print(" ".join(nombreCompleto))
 
