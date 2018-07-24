bb3d3f872d9234a434de6812675adc7dfd0e3051
Heisen14 <danilo_01@outlook.es>
2018-07-24 13:50:37 -0500

    24/07/2018

---
+++ Calculadora de propinas.py
@@ -0,0 +1,7 @@
+##Crea un programa que calcule la propina e imprima el costo total de la transacción. Considera que el subtotal es 44.50 y la propina será del 10%.
+sub=44.5
+propina=.10
+
+total=(sub*propina)+sub
+print(total)
+
---
+++ main.py
@@ -0,0 +1,9 @@
+##Un palíndromo es una palabra o frase que se lee igual al derecho y al revés, por ejemplo "anita lava la tina". Tú tarea será crear un programa que reciba una frase como input (esto está automatizado gracias a AutoGradr) y determine si esta frase es un palíndromo.
+
+palindromo=input("Escribe una frase: ")
+pal=palindromo.split()
+
+##if pal.reverse()==pal:
+##    print("La frase que ingresaste SÍ es un palindromo")
+##else:
+##    print("La frase que ingresaste NO es un palindromo")
